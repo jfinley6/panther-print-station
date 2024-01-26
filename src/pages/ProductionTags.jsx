d918cf9dff0a7742f1b2ebf545f26e28eb046088
johnfinley <johnfinleybremerton@gmail.com>
2024-01-26 16:48:42 -0700

    Created pages folder for starting elements

---
+++ src/pages/ProductionTags.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductionTags() {
+  return (
+    <div>ProductionTags</div>
+  )
+}
+
+export default ProductionTags
---
+++ src/pages/SerialNumbers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SerialNumbers() {
+  return (
+    <div>SerialNumbers</div>
+  )
+}
+
+export default SerialNumbers
