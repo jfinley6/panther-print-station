d37d5a2793aa531a1cfade9a367c0cdfde3d18f1
johnfinley <johnfinleybremerton@gmail.com>
2024-01-27 17:42:58 -0700

    App now has routes with correct paths and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import React from "react";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import Navbar from "./components/nav/Navbar";
+import SerialNumbers from "./pages/SerialNumbers";
+import ProductionTags from "./pages/ProductionTags";
+import CrateTags from "./pages/CrateTags";
+import Home from "./pages/Home";
+import RMATags from "./pages/RMATags";
 
-import './App.css'
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Navbar />}>
+            <Route index={true} element={<Home />} />
+            <Route path="serial-numbers" element={<SerialNumbers />} />
+            <Route path="production-tags" element={<ProductionTags />} />
+            <Route path="crate-tags" element={<CrateTags />} />
+            <Route path="rma-tags" element={<RMATags />} />
+        </Route>
+    )
+);
 
-function App() {
-
-  return (
-    <>
-      <div>
-        Content
-      </div>
-    </>
-  )
+function App({ routes }) {
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
-export default App
+export default App;
