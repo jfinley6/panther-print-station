6d4642c7c8cb8551cf658eb973a5fb2b82ef911f
johnfinley <johnfinleybremerton@gmail.com>
2024-02-12 20:49:38 -0700

    Imported and added PantherVideo to component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Bars from "../components/decor/Bars";
 import "../index.css";
+import PantherVideo from "../components/decor/PantherVideo";
 
 function Home() {
     return (
         <div className="main">
             <Bars />
+            <PantherVideo />
         </div>
     );
 }
