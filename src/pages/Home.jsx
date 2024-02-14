ecb01ca567b5c820cef5d5ad6da0534660f996a3
johnfinley <johnfinleybremerton@gmail.com>
2024-02-14 14:13:09 -0700

    Added Footer component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Bars from "../components/decor/Bars";
+import Footer from "../components/footer/Footer";
 import "../index.css";
 import PantherVideo from "../components/decor/PantherVideo";
 
 function Home() {
     return (
-        <div className="main">
-            <Bars />
-            <PantherVideo />
-        </div>
+        <>
+            <div className="main home">
+                <Bars />
+                <PantherVideo />
+            </div>
+            <Footer />
+        </>
     );
 }
 
