eac749a8e4513d41ca2dbca2dfe78ac5bdfb7cb9
John Finley <finjoh@IDBU.com>
2025-03-12 09:15:26 -0600

    Update main window dimensions and disable devTools
    - Changed main window dimensions from 1024x728 to 800x600 in main.ts
    - Disabled devTools on startup in main.ts
    - Updated application icon in assets/icon.png

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,10 +71,11 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 800,
+    height: 600,
     icon: getAssetPath('icon.png'),
     webPreferences: {
+      devTools: false,
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
